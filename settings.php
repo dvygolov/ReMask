d44c4ea87f06c393f7c1e89d308eb08e94484a03
maksimlucas778 <87097202+maksimlucas778@users.noreply.github.com>
2021-07-08 00:03:51 +0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,5 +1,5 @@
 <?php
-$password = "12345"; //Password to access the UI
+$password = "gg1234567899"; //Password to access the UI
 $fileName="accounts.csv"; //File name where account tokens are stored
 $baseFileName="base.txt";
 ?>
