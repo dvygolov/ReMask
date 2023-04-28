c8dff96b6326b8be4f4b7a17224a27ae8837f1d4
Daniel Vygolov <rattack@gmail.com>
2023-04-28 19:52:30 +0400

    removed junk

--- settings.php
+++ settings.php
@@ -1,5 +1,4 @@
 <?php
 $password = "12345"; //Password to access the UI
-$fileName="accounts.csv"; //File name where account tokens are stored
+$fileName="accounts.csv"; //File name where accounts are stored
 $baseFileName="base.txt";
-?>
