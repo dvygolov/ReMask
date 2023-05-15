f030d09daebe7995f55e20344002fd8085e0bb24
Daniel Vygolov <rattack@gmail.com>
2023-05-15 12:23:28 +0400

    Case-sensitive bug

--- accounts.php
+++ accounts.php
@@ -3,7 +3,7 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 require_once __DIR__ . '/checkpassword.php';
-require_once __DIR__ . '/classes/FBAccountSerializer.php';
+require_once __DIR__ . '/classes/FbAccountSerializer.php';
 require_once __DIR__ . '/classes/FbAccount.php';
 require_once __DIR__ . '/classes/RemaskProxy.php';
 
